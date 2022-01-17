9e75b8f7a5f1dcaf43b5c2618ebc982231a06a39
007steve <stephen.plummer1.sp@gmail.com>
2022-01-17 12:27:57 -0500

    updated port

--- index.js
+++ index.js
@@ -1,24 +1,24 @@
 import express, { json } from 'express';
 import conversationRoutes from './routes/conversation.js'
-import  mongoose  from 'mongoose';
-import  dotenv  from 'dotenv';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 import cors from 'cors'
 //ENV
 dotenv.config()
 
 // Setup Server
 const app = express()
-const Port = process.env.Port||8000 
+const PORT = process.env.PORT || 8000
 app.use(cors())
 app.use(json())
-app.listen(Port, () => console.log(`Server running on ${Port}`))
+app.listen(PORT, () => console.log(`Server running on ${PORT}`))
 app.use('/conversation', conversationRoutes)
 
 // MongoDB Connection
- mongoose.connect(process.env.MONGODB_STRING_URL,{
-     useNewurlParser: true,
-     useUnifiedTopology: true,
- },(err) => {
-     if (err) return console.log(err)
-     console.log("Connected to MongoDB")
- })
+mongoose.connect(process.env.MONGODB_STRING_URL, {
+    useNewurlParser: true,
+    useUnifiedTopology: true,
+}, (err) => {
+    if (err) return console.log(err)
+    console.log("Connected to MongoDB")
+})
